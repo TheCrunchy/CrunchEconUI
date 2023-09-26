0440c1cb25264010d3bdd7b82303e28b154eed29
TheCrunchy <crunchycamsta@gmail.com>
2023-09-26 08:50:49 +0100

    temp listings

--- CrunchEconUI/Services/JsonListingService.cs
+++ CrunchEconUI/Services/JsonListingService.cs
@@ -98,23 +98,23 @@ namespace CrunchEconUI.Services
             //    OwnerId = 76561198045390854,
             //    Suspended = false
             //});
-            //for (int i = 0; i < 50; i++)
-            //{
-            //    Guid Id = Guid.NewGuid();
-            //    ListedItems.Add(Id, new ItemListing()
-            //    {
-            //        ItemId = "ExampleId3",
-            //        BuyPricePerItem = 50,
-            //        SellPricePerItem = 75,
-            //        Id = Id,
-            //        IsBuying = false,
-            //        IsSelling = true,
-            //        Amount = 50,
-            //        MaxAmountToBuy = 100,
-            //        OwnerId = 76561198045390854,
-            //        Suspended = false
-            //    });
-            //}
+            for (int i = 0; i < 50; i++)
+            {
+                Guid Id = Guid.NewGuid();
+                ListedItems.Add(Id, new ItemListing()
+                {
+                    ItemId = "MyObjectBuilder_Ingot/Iron",
+                    BuyPricePerItem = 50,
+                    SellPricePerItem = 75,
+                    Id = Id,
+                    IsBuying = false,
+                    IsSelling = true,
+                    Amount = 50,
+                    MaxAmountToBuy = 100,
+                    OwnerId = 76561198045390854,
+                    Suspended = false
+                });
+            }
             return ListedItems.ToList().Select(x => x.Value).ToList();
         }
 
